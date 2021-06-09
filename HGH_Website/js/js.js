539bdb2c7e88fbdaa40d747b6af9e6035ec42362
Osamahejaz <85194535+Osamahejaz@users.noreply.github.com>
2021-06-09 12:08:06 +0300

    Add files via upload

--- HGH_Website/js/js.js
+++ HGH_Website/js/js.js
@@ -1,3 +1,15 @@
 
-new WOW().init();
 
+
+
+
+$(".navbar-toggler").click(function(){
+
+
+$(".section_table").toggle()
+$(".section_table").css(" width: 100%;")
+$(".each_setcion").hide()
+
+
+})
+new WOW().init();
